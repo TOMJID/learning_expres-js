f8e392a953b0bc51f4ebb97305a4ba62a37f77f4
tomjid <tomjidhuda04@gmail.com>
2025-10-17 12:05:59 +0600

    request object

---
+++ understanding req & res/request(req)/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+app.get("/inspect", (req, res) => {
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+  // http://localhost:3000/inspect?test=hello&t2=hi
+  console.log(`Request Query: ${req.query}`);
+  console.log(`Request headers: ${req.headers}`);
+  res.send("inspect route");
+});
+app.get("/params/:id", (req, res) => {
+  console.log(`Request Method: ${req.method}`);
+  console.log(`Request URL: ${req.url}`);
+  console.log(`Request Parameters: ${req.params.id}`);
+  res.send("param route");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
